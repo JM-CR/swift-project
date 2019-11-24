aa099ed0b8fd23d4675edb57e58f8a6fce836be0
Josue Mosh <msi_98_98@yahoo.com.mx>
2019-11-24 17:07:02 -0600

    Navigation from MyQuestionsVC to MyAnswersVC

--- you-say!/you-say!/View Controllers/Principal/MyAnswers/MyAnswersViewController.swift
+++ you-say!/you-say!/View Controllers/Principal/MyAnswers/MyAnswersViewController.swift
@@ -10,23 +10,15 @@ import UIKit
 
 class MyAnswersViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    // MARK: - Actions
     
-    @IBAction func botonRegresarPresionado(_ sender: UIButton) {
+    /**
+     Returns to the generalVC.
+     
+     - Parameter sender: Button that triggered the action.
+     */
+    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
