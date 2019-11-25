137b910b7d1bde685a9075379aa00e95cf315b8b
Josue Mosh <msi_98_98@yahoo.com.mx>
2019-11-24 20:03:51 -0600

    Add UI elements to MyAnswersVC

--- you-say!/you-say!/View Controllers/Principal/MyAnswers/MyAnswersViewController.swift
+++ you-say!/you-say!/View Controllers/Principal/MyAnswers/MyAnswersViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class MyAnswersViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: Properties
+    
+    // MARK: Core Data
+    
+    
     // MARK: - Actions
     
     /**
@@ -21,4 +30,53 @@ class MyAnswersViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+}
+
+// MARK: - TableView Data Source
+
+extension MyAnswersViewController: UITableViewDataSource {
+    
+    /**
+     Indicates the number of rows for a given section.
+     
+     - Parameter tableView: TableView object.
+     - Parameter section: Section to query.
+     - Returns: Total of rows for section.
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    /**
+     Formats a cell in the tableView.
+     
+     - Parameter tableView: TableView object.
+     - Parameter indexPath: Current position of cell.
+     - Returns: Cell ready to display.
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get reusable cell
+        let answerCell = tableView.dequeueReusableCell(withIdentifier: "answerCell", for: indexPath) as! AnswerTableViewCell
+        
+        // Get answer
+        
+        return answerCell
+    }
+    
+}
+
+// MARK: - TableView Delegate
+
+extension MyAnswersViewController: UITableViewDelegate {
+    
+    /**
+     Deselects a row when the user taps on it.
+     
+     - Parameter tableView: TableView object.
+     - Parameter indexPath: Position of the tapped cell.
+     */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
